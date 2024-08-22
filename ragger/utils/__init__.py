51b88725596bdd9ad88ed0803e5434e7254e0d7c
ArturOle <artur.oleksinski99@gmail.com>
2024-08-22 13:16:53 +0200

    OCR functionality, dokerization, restructurization

---
+++ ragger/utils/__init__.py
@@ -0,0 +1,4 @@
+from .set_config_variables import get_config_variables, set_env_variables_from_config
+from .logger_setup import setup_logger
+
+__all__ = ["get_config_variables", "set_env_variables_from_config", "setup_logger"]
