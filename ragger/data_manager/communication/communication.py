583677604ac72f687346172a2ed60054e663c943
ArturOle <artur.oleksinski99@gmail.com>
2024-08-27 18:53:59 +0200

    Connecting to neo4j

--- ragger/data_manager/communication/communication.py
+++ ragger/data_manager/communication/communication.py
@@ -1,4 +1,16 @@
+from neo4j import GraphDatabase
 
 
 class Comunicator:
-    pass
+    def __init__(self, uri, user, password):
+        self.driver = GraphDatabase.driver(uri, auth=(user, password))
+        self.driver.verify_connectivity()
+
+
+if __name__ == "__main__":
+    uri = "neo4j://localhost:7687"
+    user = "neo4j"
+    password = "neo4j"
+
+    communicator = Comunicator(uri, user, password)
+    print("Connected to Neo4j")
