7fa9b23420ef6759970ec82f28881c7a4bc81483
ArturOle <artur.oleksinski99@gmail.com>
2024-09-04 00:16:31 +0200

    improved Query Builder

--- ragger/data_manager/communicator/query_builder.py
+++ ragger/data_manager/communicator/query_builder.py
@@ -1,26 +1,46 @@
-from ragger.data_manager.data_classes import Literature, Chunk, Tag
+from ragger.data_manager.data_classes import (
+    LiteratureGraph,
+    Literature,
+    Chunk,
+    Tag,
+    RelationWeight
+)
 
 
 class QueryBuilder:
 
     @staticmethod
+    def add_literature_subgraph(literature_graph: LiteratureGraph):
+        pass
+
+    @staticmethod
+    def add_relation_literature_chunk(
+            literature: Literature, chunk: Chunk
+    ):
+        return (
+            f"MATCH (l:Literature) WHERE l.filename = '{literature.filename}'",
+            f"MATCH (c:Chunk) WHERE c.text = '{chunk.text}'",
+            "MERGE (l)<-[:PART_OF]-(c)"
+        )
+
+    @staticmethod
     def add_literature(literature: Literature):
-        return f"MERGE (a:Literature {literature.to_dict()})"
+        return f"MERGE (a:Literature {{{literature.to_dict()}}})"
 
     @staticmethod
     def add_chunk(chunk: Chunk):
-        return f"MERGE (a:Chunk {chunk.to_dict()})"
-
-    @staticmethod
-    def relation_between_chunk_and_literature(
-            chunk: Chunk,
-            literature: Literature
-    ):
-        match_chunk = f"MATCH (a:Chunk {chunk.to_dict()})"
-        match_literature = f"MATCH (b:Literature {literature.to_dict()})"
-        merge = "MERGE (a)-[:CITES]->(b)"
-        return f"{match_chunk}, {match_literature}, {merge}"
+        return f"MERGE (a:Chunk {{{chunk.to_dict()}}})"
 
     @staticmethod
     def add_tag(tag: Tag):
-        return f"MERGE (a:Tag {tag.to_dict()})"
+        return f"MERGE (a:Tag{{{tag.to_dict()}}})"
+
+    @staticmethod
+    def add_relation_literature_tag(
+            literature: Literature, tag: Tag, RelationWeight: RelationWeight
+    ):
+        return (
+            f"MATCH (l:Literature) WHERE l.filename = '{literature.filename}'",
+            f"MATCH (t:Tag) WHERE t.text = '{tag.text}'",
+            f"MERGE (l)-[:RELATED_TO{{{RelationWeight.weight}}}]->(t)"
+        )
