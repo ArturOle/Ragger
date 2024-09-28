25f764e5a7a368241337546d020d0de2ccef19a9
ArturOle <artur.oleksinski99@gmail.com>
2024-09-28 15:59:17 +0200

    Cleanup of text splitter

--- ragger/data_manager/preprocessor/text_splitter.py
+++ ragger/data_manager/preprocessor/text_splitter.py
@@ -1,16 +1,9 @@
 import numpy as np
-from functools import singledispatchmethod
-from copy import copy
 import re
 
 from abc import ABC, abstractmethod
-
-
-def find_end_of_sentence(text, start):
-    end = start
-    while end < len(text) and text[end] not in '.!?':
-        end += 1
-    return end
+from copy import copy
+from functools import singledispatchmethod
 
 
 class AbstractSplitter(ABC):
@@ -39,14 +32,24 @@ class TextSplitter(AbstractSplitter):
 
     @staticmethod
     def _overlap_splits(text_length, chunk_size, overlap):
-        return np.arange(chunk_size, text_length-chunk_size+1, chunk_size-overlap)
+        return np.arange(
+            chunk_size,
+            text_length-chunk_size+1,
+            chunk_size-overlap
+        )
 
     @singledispatchmethod
     def _split_dispatcher(self, overlap, text, chunk_size, margin):
         raise ValueError(f"Invalid overlap type: {type(overlap)}")
 
     @singledispatchmethod
-    def _overlap_splits_dispatcher(self, margin, overlap, data_size, chunk_size):
+    def _overlap_splits_dispatcher(
+        self,
+        margin,
+        overlap,
+        data_size,
+        chunk_size
+    ):
         raise ValueError(f"Invalid overlap type: {type(margin)}")
 
     @_overlap_splits_dispatcher.register
@@ -71,7 +74,9 @@ class TextSplitter(AbstractSplitter):
         while True:
             text_length = len(remaining_text)
             static_split_pos = text_length - max_chunk_size
-            positive_margin_subset = text[static_split_pos:static_split_pos+margin]
+            positive_margin_subset = text[
+                static_split_pos:static_split_pos+margin
+            ]
             new_pos = self.split_pos(positive_margin_subset, static_split_pos)
             split_positions.insert(0, (new_pos, len(remaining_text)+overlap))
             remaining_text = remaining_text[:new_pos]
@@ -81,17 +86,21 @@ class TextSplitter(AbstractSplitter):
 
         split_positions.insert(0, (0, len(remaining_text)+overlap))
 
-        return {key:text[i:j] for key, (i, j) in enumerate(split_positions)}
+        return {key: text[i:j] for key, (i, j) in enumerate(split_positions)}
 
     @_split_dispatcher.register
     def _(self, overlap: None, text, chunk_size, margin):
         split_pos = self.splits(len(text), chunk_size)
         split_pos = np.insert(split_pos, 0, 0)
-        return {key:text[i:i+chunk_size] for key, i in enumerate(split_pos)}
+        return {key: text[i:i+chunk_size] for key, i in enumerate(split_pos)}
 
     @_split_dispatcher.register
     def _(self, overlap: float, text, chunk_size, margin):
-        return self.overlap_splits(len(text), chunk_size, int(chunk_size*overlap))
+        return self.overlap_splits(
+            len(text),
+            chunk_size,
+            int(chunk_size*overlap)
+        )
 
     def split_pos(self, string, current_position):
         for i, letter in enumerate(string):
@@ -99,11 +108,13 @@ class TextSplitter(AbstractSplitter):
                 return current_position + i + 1
 
         for i, letter in enumerate(string):
-            if not isinstance(self.white_space_pattern.match(letter), type(None)):
+            if not isinstance(
+                self.white_space_pattern.match(letter),
+                type(None)
+            ):
                 return current_position + i + 1
 
         return current_position
-
 
     def split_neg(self, string, current_position):
         inv_string = string[::-1]
@@ -112,13 +123,27 @@ class TextSplitter(AbstractSplitter):
                 return current_position + (len(string)-i)
 
         for i, letter in enumerate(inv_string):
-            if not isinstance(self.white_space_pattern.match(letter), type(None)):
+            if not isinstance(
+                self.white_space_pattern.match(letter),
+                type(None)
+            ):
                 return current_position + (len(string)-i)
 
         return current_position
 
-    def split(self, text, chunk_size=100, overlap: None = None, margin: int = 10):
+    def split(
+        self,
+        text,
+        chunk_size=100,
+        overlap: None = None,
+        margin: int = 10
+    ):
         return self._split_dispatcher(overlap, text, chunk_size, margin)
 
     def overlap_splits(self, text_length, chunk_size, overlap, margin=None):
-        return self._overlap_splits_dispatcher(margin, overlap, text_length, chunk_size)
+        return self._overlap_splits_dispatcher(
+            margin,
+            overlap,
+            text_length,
+            chunk_size
+        )
