2aedf7d5f07b6223d731fa1dc00270b482b4a1e2
ArturOle <artur.oleksinski99@gmail.com>
2024-10-01 21:53:38 +0200

    Moving new algorithm to codebase

--- ragger/data_manager/preprocessor/text_splitter.py
+++ ragger/data_manager/preprocessor/text_splitter.py
@@ -1,13 +1,11 @@
-import numpy as np
+from functools import singledispatchmethod
 import re
 
 from abc import ABC, abstractmethod
-from copy import copy
-from functools import singledispatchmethod
 
 
 class AbstractSplitter(ABC):
-    """ This is how DRY dies... with thunderous applaus of abstract OOP """
+    """ This is how DRY dies, with thunderous applaus of abstract OOP """
     @abstractmethod
     def split(self, data, chunk_size, overlap, margin):
         pass
@@ -24,19 +22,16 @@ class AbstractSplitter(ABC):
 
 
 class TextSplitter(AbstractSplitter):
+    """ The text splitter for dividing text into chunks """
     white_space_pattern = re.compile('\s')
 
     @staticmethod
     def splits(text_length, chunk_size):
-        return np.arange(chunk_size, text_length-chunk_size+1, chunk_size)
+        raise NotImplementedError
 
     @staticmethod
     def _overlap_splits(text_length, chunk_size, overlap):
-        return np.arange(
-            chunk_size,
-            text_length-chunk_size+1,
-            chunk_size-overlap
-        )
+        raise NotImplementedError
 
     @singledispatchmethod
     def _split_dispatcher(self, overlap, text, chunk_size, margin):
@@ -44,63 +39,66 @@ class TextSplitter(AbstractSplitter):
 
     @singledispatchmethod
     def _overlap_splits_dispatcher(
-        self,
-        margin,
-        overlap,
-        data_size,
-        chunk_size
+            self, margin, overlap, data_size, chunk_size
     ):
         raise ValueError(f"Invalid overlap type: {type(margin)}")
 
     @_overlap_splits_dispatcher.register
     def _(self, margin: None, overlap: int, data_size, chunk_size):
-        return TextSplitter.overlap_splits(data_size, chunk_size, overlap)
+        raise NotImplementedError
 
     @_overlap_splits_dispatcher.register
     def _(self, margin: int, overlap: int, data_size, chunk_size):
-        overlaps = []
+        raise NotImplementedError
+
+    # TODO: Task: Break this down into smaller functions
+    # Description: The split function is too long and should be broken down
+    # into smaller, more manageable functions. This will iprove readablity and
+    # maintainability of the code. Tags: refactor
 
     @_split_dispatcher.register
     def _(self, overlap: int, text, chunk_size, margin):
         max_chunk_size = chunk_size - overlap
-        remaining_text = copy(text)
-        split_positions = []
-        text_length = len(remaining_text)
-        static_split_pos = text_length - max_chunk_size
+        remaining_text_length = len(text)
+
+        static_split_pos = remaining_text_length - max_chunk_size
         positive_margin_subset = text[static_split_pos:static_split_pos+margin]
         new_pos = self.split_pos(positive_margin_subset, static_split_pos)
-        split_positions.insert(0, (new_pos, len(remaining_text)))
-        remaining_text = remaining_text[:new_pos]
+        split_positions = [text[new_pos:remaining_text_length]]
+        remaining_text_length = remaining_text_length -\
+            (remaining_text_length-new_pos)
         while True:
-            text_length = len(remaining_text)
-            static_split_pos = text_length - max_chunk_size
+            static_split_pos = remaining_text_length - max_chunk_size
+            negative_margin_subset = text[
+                remaining_text_length-margin:remaining_text_length
+            ]
+            new_neg_pos = self.split_neg(
+                negative_margin_subset,
+                remaining_text_length
+            )
+
             positive_margin_subset = text[
                 static_split_pos:static_split_pos+margin
             ]
             new_pos = self.split_pos(positive_margin_subset, static_split_pos)
-            split_positions.insert(0, (new_pos, len(remaining_text)+overlap))
-            remaining_text = remaining_text[:new_pos]
+            split_positions.append(text[new_pos:new_neg_pos])
+            remaining_text_length = remaining_text_length -\
+                (remaining_text_length-new_pos)
 
-            if len(remaining_text) < max_chunk_size:
+            if remaining_text_length < max_chunk_size:
                 break
 
-        split_positions.insert(0, (0, len(remaining_text)+overlap))
+        split_positions.append(text[0:remaining_text_length+overlap])
 
-        return {key: text[i:j] for key, (i, j) in enumerate(split_positions)}
+        return split_positions
 
     @_split_dispatcher.register
     def _(self, overlap: None, text, chunk_size, margin):
-        split_pos = self.splits(len(text), chunk_size)
-        split_pos = np.insert(split_pos, 0, 0)
-        return {key: text[i:i+chunk_size] for key, i in enumerate(split_pos)}
+        raise NotImplementedError
 
     @_split_dispatcher.register
     def _(self, overlap: float, text, chunk_size, margin):
-        return self.overlap_splits(
-            len(text),
-            chunk_size,
-            int(chunk_size*overlap)
-        )
+        raise NotImplementedError
 
     def split_pos(self, string, current_position):
         for i, letter in enumerate(string):
@@ -120,30 +118,27 @@ class TextSplitter(AbstractSplitter):
         inv_string = string[::-1]
         for i, letter in enumerate(inv_string):
             if letter == '.':
-                return current_position + (len(string)-i)
+                return current_position - i
 
         for i, letter in enumerate(inv_string):
             if not isinstance(
                 self.white_space_pattern.match(letter),
                 type(None)
             ):
-                return current_position + (len(string)-i)
+                return current_position - i
 
         return current_position
 
     def split(
-        self,
-        text,
-        chunk_size=100,
-        overlap: None = None,
-        margin: int = 10
+            self,
+            text,
+            chunk_size=100,
+            overlap: None = None,
+            margin: int = 10
     ):
         return self._split_dispatcher(overlap, text, chunk_size, margin)
 
     def overlap_splits(self, text_length, chunk_size, overlap, margin=None):
         return self._overlap_splits_dispatcher(
-            margin,
-            overlap,
-            text_length,
-            chunk_size
+            margin, overlap, text_length, chunk_size
         )
